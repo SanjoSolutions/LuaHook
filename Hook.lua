96006f1a46d19cffe506d65c9ed74d9b19baad04
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-23 18:42:01 +0100

    Revert "Work"
    This reverts commit 73f8199926d07ea9f29dad8f2ec953c68d202f96.

--- Hook.lua
+++ Hook.lua
@@ -1,47 +1,39 @@
-local addOnName = ...
-local version = '1.0.0'
+Hook = Hook or {}
+local addOnName, AddOn = ...
+local _ = {}
 
-if not Library.isRegistered(addOnName, version) then
-  --- @class Hook
-  local Hook = Hook or {}
+Hook.Hook = {}
 
-  Library.register(addOnName, version, Hook)
-  local addOnName, AddOn = ...
-  local _ = {}
+function Hook.Hook:new()
+  local hook = {
+    _callbacks = {}
+  }
+  setmetatable(hook, { __index = Hook.Hook })
+  return hook
+end
 
-  Hook.Hook = {}
-
-  function Hook.Hook:new()
-    local hook = {
-      _callbacks = {}
-    }
-    setmetatable(hook, { __index = Hook.Hook })
-    return hook
-  end
-
-  function Hook.Hook:registerCallback(callback)
-    local object = {
-      callback = callback
-    }
-    table.insert(self._callbacks, object)
-    local hasCallbackBeenUnregistered = false
-    local handle = {
-      unregisterCallback = function ()
-        if not hasCallbackBeenUnregistered then
-          local index = Array.indexOf(self._callbacks, object)
-          if index ~= -1 then
-            table.remove(self._callbacks, index)
-          end
-          hasCallbackBeenUnregistered = true
+function Hook.Hook:registerCallback(callback)
+  local object = {
+    callback = callback
+  }
+  table.insert(self._callbacks, object)
+  local hasCallbackBeenUnregistered = false
+  local handle = {
+    unregisterCallback = function ()
+      if not hasCallbackBeenUnregistered then
+        local index = Array.indexOf(self._callbacks, object)
+        if index ~= -1 then
+          table.remove(self._callbacks, index)
         end
+        hasCallbackBeenUnregistered = true
       end
-    }
-    return handle
-  end
-
-  function Hook.Hook:runCallbacks(...)
-    for _, object in ipairs(self._callbacks) do
-      object.callback(...)
     end
+  }
+  return handle
+end
+
+function Hook.Hook:runCallbacks(...)
+  for _, object in ipairs(self._callbacks) do
+    object.callback(...)
   end
 end
